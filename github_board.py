3ea95b4aa3e91696ca18130ede3e0a29da46c27f
Alexander Bayandin <a.bayandin@gmail.com>
2014-01-23 17:25:41 +0400

    Testability (#2)

--- github_board.py
+++ github_board.py
@@ -22,26 +22,31 @@ UTC_TO_PST_OFFSET = 28800  # seconds in 8 hours
 
 def board_origin(today):
     """
+    Calculates point 0×0 (top left corner of the board) in seconds (in PST)
+
     :type today: datetime.date
     :rtype: int
     """
-
-    def offset(date):
-        """
-        :type date: datetime.date
-        :rtype: int
-        """
-        return 7 - (datetime.date.weekday(date) + 1) % 7
-
     last_cell_dt = today
     first_cell_dt = datetime.date(last_cell_dt.year - 1, last_cell_dt.month, last_cell_dt.day)
     first_cell_ux = time.mktime(first_cell_dt.timetuple()) - time.timezone + UTC_TO_PST_OFFSET  # localtime → PST
-    first_week_offset = offset(first_cell_dt) * STEP
-    return int(first_cell_ux) + first_week_offset
+    return int(first_cell_ux) + sunday_offset(first_cell_dt)
+
+
+def sunday_offset(date):
+    """
+    Calculates time from date to the next sunday in seconds
+
+    :type date: datetime.date
+    :rtype: int
+    """
+    return (7 - (datetime.date.weekday(date) + 1) % 7) * STEP
 
 
 def template_to_tape(template, origin):
     """
+    Converts template to tape of timestamps
+
     :type template: list of list of int
     :type origin: int
     :rtype: list of int
@@ -57,15 +62,24 @@ def template_to_tape(template, origin):
     return tape
 
 
-def load_template(filepath):
+def load_template(file_path):
     """
-    :type filepath: str
+    Loads template from file
+
+    :type file_path: str
     :rtype: list of list of int
     """
     template = []
-    f = file(filepath, "r")
-    for l in f.readlines():
-        template.append(map(int, list(l.strip())))
+    f = open(file_path, "r")
+    l = []
+    for c in f.read():
+        if c in ("0", "1"):
+            l.append(int(c))
+        elif c == "\n" and l:
+            template.append(l)
+            l = []
+    if l:
+        template.append(l)
     f.close()
     return template
 
