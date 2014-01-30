ea1b40acf3ba42a064c306c4ce630f8ee5b9ee1c
Alexander Bayandin <a.bayandin@gmail.com>
2014-01-30 21:21:08 +0400

    Fix docstring

--- github_board.py
+++ github_board.py
@@ -36,7 +36,7 @@ def board_origin(today):
 
 def sunday_offset(date, reverse=False):
     """
-    Calculates time from date to the next or previous sunday in seconds
+    Calculates time from date to the next sunday or previous saturday in seconds
 
     :type date: datetime.date
     :type reverse: bool
