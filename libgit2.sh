65602766dd51733aa0b3630bc6fabae4dd528eac
Alexander Bayandin <bayandin@users.noreply.github.com>
2014-02-04 20:52:20 +0300

    Update libgit2.sh

--- libgit2.sh
+++ libgit2.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-cd ~
-wget https://github.com/libgit2/libgit2/archive/v0.20.0.tar.gz
-tar -xf v0.20.0.tar.gz
-mv libgit2{-0.20.0,} && cd libgit2
-mkdir build && cd build
+
+LIBGIT2_VERSION="0.20.0"
+
+cd ~/
+wget https://github.com/libgit2/libgit2/archive/v${LIBGIT2_VERSION}.tar.gz
+tar -xf v${LIBGIT2_VERSION}.tar.gz
+mv libgit2{-${LIBGIT2_VERSION},}
+
+cd libgit2
+mkdir build && cd $_
 cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DBUILD_CLAR=OFF
 cmake --build . --target install
